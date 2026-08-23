c01d83a303dddcff4bbb6de7a3c29a7ba8e14941
vampirdollie <evv0101prem@gmail.com>
2026-08-22 20:49:42 -0500

    Update koala lost message caption

--- main.py
+++ main.py
@@ -646,7 +646,7 @@ async def koala(update: Update, context: ContextTypes.DEFAULT_TYPE):
         chat_id=int(KOALA_CHAT_ID),
         photo=open("koala.jpg", "rb"),
         caption=(
-            "────۶ৎ 🐨 ¡KOALA PERDIDO!────\n\n"
+            "۫ ׅ ¡KOALA PERDIDO! ੭﹕﹒\n"
             "Oh, parece que el koala se ha escapado..\n\n"
             "¡Atrápalo antes que los demás! ₍₍⚞(˶>ᗜ<˶)⚟⁾⁾"
         ),
